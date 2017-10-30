9e0aed28530ece9bf65f5d63edfbb84f353a86f3
Erick <erick.jimenez@alumnos.udg.mx>
2017-10-30 11:28:31 -0600

    Algo de js

--- js/validarForm.js
+++ js/validarForm.js
@@ -1,18 +1,66 @@
+var expr = /^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9_\-]+\.[a-zA-Z0-9_\-\.]+$/;
+var regNombre = /^[a-zA-Z ]+$/;
+
 $(document).ready(function(){
 
-  $("#nombre").focusout(function(){
+  $("#crear").click(function(){
     var nombre = $("#nombre").val();
+    var apellido = $("#ap").val();
+    var valCorreo = $("#correo").val();
+    var pwd = $("#contrasena").val();
+    var confirmar = $("conf_contrasena").val();
+    var sel = $("#uni option:selected");
 
     if (nombre == ""){
       $("#error1").fadeIn();
       return false;
     }
-    else if (nombre == "m") {
-      $("#error1").fadeIn();
+
+    else if (nombre.length < 3 || !regNombre.test(nombre)) {
+      $("#error2").fadeIn();
+      return false;
+    }
+
+    else {
+      $("error1").fadeOut();
+
+    if (apellido == "") {
+      $("#errorap").fadeIn();
+      return false;
+    }
+    else if (apellido.length < 3 || !regNombre.test(apellido)) {
+      $("#error2").fadeIn();
       return false;
     }
     else {
-      $("error1").fadeOut();
+       $("errorap").fadeOut();
+    if (valCorreo == "" || !expr.test(valCorreo)){
+      $("#errorCorreo").fadeIn();
+      return false;
     }
+    else {
+      $("#errorCorreo").fadeOut();
+
+    if (pwd == "" || pwd.length < 8){
+      $("errorCont").fadeIn();
+      return false;
+    }
+    else {
+      $("#errorCont").fadeOut();
+    if (regContr.test(confirmar) == contr){
+      $("coinciden").fadeIn();
+      return false;
+    }
+    else {
+      $("errorConf").fadeIn();
+
+    if (sel.val() == ""){
+      $("#selUni").fadeIn();
+    }
+  }
+}
+}
+}
+}
   });
 });
